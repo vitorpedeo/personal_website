7c0893eeefb24afced0e6a356567d332805f330f
Vitor Pereira <vitorpereiradeoli@gmail.com>
2024-03-02 15:36:57 -0300

    :fire: remove old files

--- src/modules/common/components/InlineCode.tsx
+++
@@ -1,5 +0,0 @@
-import { Code, CodeProps } from '@chakra-ui/react';
-
-export function InlineCode({ children }: CodeProps) {
-  return <Code>{children}</Code>;
-}
--- src/modules/common/lib/translations/index.ts
+++
@@ -1,7 +0,0 @@
-import { enUS } from './en-US';
-import { ptBR } from './pt-BR';
-
-export const localesMessages = {
-  'en-US': enUS,
-  'pt-BR': ptBR,
-};
